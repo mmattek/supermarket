6f63554b450acef37adb6f0845e4634779c207f6
mitch <mitch@mattek.net>
2014-12-18 20:53:01 -0600

    making Pricer and abstract class instead of interface to share code

--- src/main/java/techTest/pricers/Pricer.java
+++ src/main/java/techTest/pricers/Pricer.java
@@ -4,19 +4,46 @@ package techTest.pricers;
  * Abstracts away pricing of an individual items
  * @author mmattek
  */
-public interface Pricer {
+public abstract class Pricer {
 
+	private final Character letter;
+	private int pricePerUnit;
 	
-	public Character characterOfThisPricer();
+	// No args constructor is a no-no
+	// but hide in the implementing classes
+	public Pricer(){
+		letter =null;
+		pricePerUnit=0;
+	}
 	
-	public String stringOfThisPricer();
 	
 	/**
-	 * @param theOrderString (entireOrder, in case entireContext is required
-	 * its passed each time so a pricer for product can be reused
-	 * across multiple orders 
-	 * @param numberOfItmes - the number Of Item to price
-	 * @return
+	 * easier constructor to take String,
+	 * Note if you happen to pass a string longer than a letter, takes the first
+	 * @param letterVal
+	 * @param pricePerUnit -- this is the standard, non bulk discounted price
 	 */
-	public int price( String orderString); 
+	public Pricer(String letterVal, int pricePerUnit){
+		this(letterVal.charAt(0), pricePerUnit);
+		
+	}
+	
+	public Pricer (Character letter, int pricePerUnit) {
+		this.letter=letter;
+		this.pricePerUnit=pricePerUnit;
+	}
+
+	public Character characterOfThisPricer() {
+		return letter;
+	}
+
+	public String stringOfThisPricer() {
+		return String.valueOf(letter);
+	}
+
+	public int getPricePerUnit(){
+		return pricePerUnit;
+	}
+	
+	public abstract int price( String orderString); 
 }
--- src/main/java/techTest/pricers/StandardPricer.java
+++ src/main/java/techTest/pricers/StandardPricer.java
@@ -2,39 +2,13 @@ package techTest.pricers;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class StandardPricer implements Pricer{
+public class StandardPricer extends Pricer{
 
-	private final Character letter;
-	private int pricePerUnit;
-	
-	
-	/**
-	 * easier constructor to take String,
-	 * Note if you happen to pass a string longer than a letter, takes the first
-	 * @param letterVal
-	 * @param pricePerUnit
-	 */
-	public StandardPricer(String letterVal, int pricePerUnit){
-		this(letterVal.charAt(0), pricePerUnit);
-		
-	}
-	
-	public StandardPricer (Character letter, int pricePerUnit) {
-		this.letter=letter;
-		this.pricePerUnit=pricePerUnit;
+
+	public StandardPricer(String string, int pricePerUnit) {
+		super(string, pricePerUnit);
 	}
 
-	@Override
-	public Character characterOfThisPricer() {
-		return letter;
-	}
-
-	@Override
-	public String stringOfThisPricer() {
-		return String.valueOf(letter);
-	}
-
-	
 	/**
 	 * The orderString is passed here so this pricer can be used multiple
 	 * times by passing the context each time
@@ -45,7 +19,7 @@ public class StandardPricer implements Pricer{
 	public int price(String orderString) {
 
 		int numberOfItems = StringUtils.countMatches(orderString, stringOfThisPricer() );
-		return numberOfItems * pricePerUnit;
+		return numberOfItems * getPricePerUnit();
 	}
 	
 	
